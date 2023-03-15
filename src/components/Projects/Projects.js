60d709a72c95f6bc043b8c4aa39cfdbe13e89756
kedar05121998 <101327577+kedar05121998@users.noreply.github.com>
2023-03-15 23:53:58 +0530

    updated code

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,30 +1,29 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
-import betterCloud from "../../Assets/project/betterCloud.png"
-import dotandkey from "../../Assets/project/dotandkey.png"
+import betterCloud from "../../Assets/project/betterCloud.png";
+import dotandkey from "../../Assets/project/dotandkey.png";
 
-import weather from "../../Assets/project/weather.png"
-import tripoto from "../../Assets/project/tripoto.png"
-import shop from "../../Assets/project/shop.png"
-import ndtv from "../../Assets/project/ndtv.png"
-
+import weather from "../../Assets/project/weather.png";
+import tripoto from "../../Assets/project/tripoto.png";
+import shop from "../../Assets/project/shop.png";
+import ndtv from "../../Assets/project/ndtv.png";
+import mind from "../../Assets/project/mind.png";
 
 function Projects() {
   return (
-    <Container  fluid className="project-section">
+    <Container fluid className="project-section">
       <div id="project" className="projects-sections"></div>
- 
+
       <Container>
-        <h1 className="project-heading" >
+        <h1 className="project-heading">
           My Recent <strong className="purple">Works </strong>
         </h1>
         <p style={{ color: "white" }}>
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-
-        <Col md={4} className="project-card">
+          <Col md={4} className="project-card">
             <ProjectCard
               imgPath={shop}
               isBlog={false}
@@ -32,11 +31,11 @@ function Projects() {
               description="This is a clone of Shop.com . this e-commerce website. This is a American base web site. In this project i have been work for 5 days with 5 teammates including me."
               stacks="Tech Stack : ReactJS,NodeJS,MongoDB,ExpressJS,Javascript,Chakra UI "
               ghLink="https://github.com/yadav123yo/FaydaBazar"
-              demoLink="https://fayda-bazar.vercel.app/"              
+              demoLink="https://fayda-bazar.vercel.app/"
             />
           </Col>
 
-        <Col md={4} className="project-card">
+          <Col md={4} className="project-card">
             <ProjectCard
               imgPath={tripoto}
               isBlog={false}
@@ -44,23 +43,23 @@ function Projects() {
               description="Tripoto-Clone-Project: is a travel community and travel search engine portal based on user-generated content. It allows users to write travel stories and discover those written by other travelers."
               stacks="Tech Stack : ReactJS,NodeJS,MongoDB,ExpressJS,Javascript,Chakra UI "
               ghLink="https://github.com/atul85400krishna/wasteful-cast-8657"
-              demoLink="https://precious-biscotti-a1a839.netlify.app/"              
+              demoLink="https://precious-biscotti-a1a839.netlify.app/"
             />
           </Col>
-{/* 
-        <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={fabbag}
-              isBlog={false}
-              title="fabbag"
-              description="fabbag is an E-commerce website. Have built the user flow of the website from sign up to placing the orders with cart updation feature"
-              stacks="Tech Stack : React, Javascript, Chakra-UI,Bootstrap"
-              ghLink="https://github.com/kedar05121998/crazy-name-9363"
-              demoLink="https://fab-bag-fawn.vercel.app/"
-            />
-          </Col> */}
 
-<Col md={4} className="project-card">
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={mind}
+              isBlog={false}
+              title="School Management System"
+              description="This is a competitive application created for the Masai Hackathon program. This SAAS web application is all about education platforms."
+              stacks="Tech Stack : JavaScript, React, Next JS, Chakra UI, Node JS, Mongo DB"
+              ghLink="https://github.com/H-unique245/MindInstallers"
+              demoLink="https://hackathon-sms.vercel.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
             <ProjectCard
               imgPath={ndtv}
               isBlog={false}
@@ -72,7 +71,7 @@ function Projects() {
             />
           </Col>
 
-        <Col md={4} className="project-card">
+          <Col md={4} className="project-card">
             <ProjectCard
               imgPath={betterCloud}
               isBlog={false}
@@ -83,7 +82,7 @@ function Projects() {
               demoLink="https://legendary-dango-0ff999.netlify.app/"
             />
           </Col>
-<Col md={4} className="project-card">
+          <Col md={4} className="project-card">
             <ProjectCard
               imgPath={dotandkey}
               isBlog={false}
@@ -93,11 +92,9 @@ function Projects() {
               ghLink="https://github.com/KaranJoshi4357/Dot-Key__Clone"
               demoLink="https://dotandkey-clone.netlify.app"
             />
-    </Col> 
+          </Col>
 
-        
-
-          <Col md={4} className="project-card">
+          {/* <Col md={4} className="project-card">
             <ProjectCard
               imgPath={weather}
               isBlog={false}
@@ -107,12 +104,7 @@ function Projects() {
               ghLink="https://github.com/kedar05121998/Weather-App/tree/main"
               demoLink="https://keen-nasturtium-8da81a.netlify.app/"
             />
-          </Col>
-
-          
-
-         
-
+          </Col> */}
         </Row>
       </Container>
     </Container>
